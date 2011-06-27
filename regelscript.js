4dec134ecebea5e6d5d5b395c00ce6301de7a118
moritz <moritz@20.178.168.192.in-addr.arpa>
2011-06-27 21:23:54 +0200

    created compiling instructions

--- regelscript.js
+++ regelscript.js
@@ -1,3 +1,11 @@
+/*
+Name: In der Regel sind die 90er vorbei Extension
+Author: Gelbstich
+Version: 0.1 alpha
+https://bitbucket.org/moritz/in-der-regel-sind-die-90er-vorbei/
+BSD-License
+*/
+
 $(document).ready(function(){
 
 // create a wrapper div for centering the page
